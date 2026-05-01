d42dce7bfcb164d470051c27864f2c55d3ff7a37
Tylerxu8 <tylerxu8888@gmail.com>
2026-05-01 15:27:34 -0700

    first commit: hello world

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = input("What's your name ? ")
+number = input("What's your favorite number? ")
+print("Hello,", name)
+print("Favorite number,", number)
+if int(number) > 100: 
+	print("That's a BIG one. ")
+else: 
+	print("Modest choice. ")
