e3c79dca76741bde7736b66e8105df626972165a
Tylerxu8 <tylerxu8888@gmail.com>
2026-05-01 15:34:04 -0700

    interactive hello with name and number

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ name = input("What's your name ? ")
 number = input("What's your favorite number? ")
 print("Hello,", name)
 print("Favorite number,", number)
-if int(number) > 100: 
+if int(number) > 50 + 50: 
 	print("That's a BIG one. ")
 else: 
 	print("Modest choice. ")
