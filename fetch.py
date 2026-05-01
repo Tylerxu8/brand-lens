b977cdc6a46efa9d376bab7978c321b1c8a82d77
Tylerxu8 <tylerxu8888@gmail.com>
2026-05-01 16:21:57 -0700

    add fetch script: url -> title in jsonl

--- fetch.py
+++ fetch.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import json
 from datetime import datetime
 
-url = "https://www.youtube.com"
+url = "https://www.apple.com"
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
